c8dd6b5a5918a22be7e4f97bd12d29fd0514eab8
AJaquier <alex.jaquier@gmail.com>
2022-10-13 19:43:05 +0200

    feat: some modifications

--- src/game.js
+++ src/game.js
@@ -1,9 +1,1 @@
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Couchfriends
- * www.couchfriends.com
- *
- * This file is used automatically include all js files in the build en src.
- */
 Game.init();
